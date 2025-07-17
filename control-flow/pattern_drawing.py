be4a90a1193f03702ddb31a8f51ea05a8e5dd16d
Mafika <rudolphmafika@gmail.com>
2025-07-17 13:11:22 +0200

    Cotrol-flow task3

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+try:
+    size = int(input("Enter the size of the pattern: "))
+    
+    count = 0
+    while(count < size):
+
+        for i in range(size):
+            print("*", end="")
+        count += 1
+        print()
+except:
+    print("Please enter a number")
