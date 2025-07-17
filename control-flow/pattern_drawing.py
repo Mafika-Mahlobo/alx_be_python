198db9d8c45a8ba2af432618d6b285e3e9a91e74
Mafika <rudolphmafika@gmail.com>
2025-07-17 13:20:36 +0200

    Error fix - task3

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,7 @@ try:
     size = int(input("Enter the size of the pattern: "))
     
     count = 0
-    while(count < size):
+    while count < size:
 
         for i in range(size):
             print("*", end="")
