7b29846f2a1683e7d27f018af55cde3f3e5b6a47
Mafika <rudolphmafika@gmail.com>
2025-07-19 13:56:03 +0200

    Fix error. Task4

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,25 @@
 try:
-    task = input("Enter your task: ").lower()
+    task = input("Enter your task: ")
     priority = input("Priority (high/medium/low): ").lower()
     time_bound = input("Is it time-bound? (yes/no): ").lower()
+    time_bound_message = "that requires immediate attention today!"
 
-    if time_bound == 'yes':
-        print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-    else:
-        print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
-    
+    match priority:
+        case "high":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            else:
+                print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+        case "medium":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            else:
+                print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+        case "low":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
+            else:
+                print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+            
 except:
     print("Invalid Value.")
