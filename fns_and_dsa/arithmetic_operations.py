8e37f7bb4e51c863360f5198d968673e1c0753c1
Mafika <rudolphmafika@gmail.com>
2025-07-25 07:05:58 +0200

    Error fix, Task0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,9 @@ def perform_operation(num1, num2, operation):
                 return num1 * num2
             case "divide":
                 if num2 == 0:
-                    return "Error: Could not divide by 0"
+                    return "Could not divide by 0"
+                elif not isinstance(num2, (int, float)):
+                    return "both numbers must be integers"
                 return num1 / num2
 
     except ValueError as e:
