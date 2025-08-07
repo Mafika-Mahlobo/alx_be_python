a30eec2e318f3fdd309919434f6ee6b75cbcd680
Mafika <rudolphmafika@gmail.com>
2025-08-07 20:43:16 +0200

    more OOP, Task1 edit

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,6 +3,10 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+
+    def __str__(self):
+        return f"Title: {self.title}. Author: {self.author}."
         
         
 class EBook(Book):
@@ -11,11 +15,17 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"Title: {self.title}. Author: {self.author}. File size: {self.file_size}"
+
 class PrintBook(Book):
     
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"Title: {self.title}. Author: {self.author}. Page Count: {self.page_count}"
 
 class Library:
     
